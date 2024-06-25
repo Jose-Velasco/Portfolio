1c79d847607235d9ff20eb4218310acef9a2ce98
Jose-Velasco <jose.juarez.velasco@gmail.com>
2024-06-24 22:35:23 -0700

    rendering default closing modal button can be toggled, project-detail component close button not working
    child modal component can now toggle showing default modal close button in the event the modal has its own close mechinism

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Project } from '../shared/project.model';
 import { ProjectsService } from '../services/projects.service';
 import { Subscription } from 'rxjs';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-project-detail',
@@ -9,11 +10,14 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./project-detail.component.scss']
 })
 export class ProjectDetailComponent implements OnInit, OnDestroy {
+  showRenderDefaultCloseButton: boolean = false;
   isLoading: boolean = true;
   videoURL: string;
   projectDetails: Project;
   projectSelectedIndex: number;
   private projectSelectedSub: Subscription;
+  @Output() closeModal = new EventEmitter<null>();
+  @Output() renderDefaultCloseButton = new EventEmitter<boolean>();
   constructor(private projectService: ProjectsService) { }
 
   ngOnInit(): void {
@@ -25,6 +29,11 @@ export class ProjectDetailComponent implements OnInit, OnDestroy {
         this.videoURL = this.projectDetails.videoURL;
         this.isLoading = false;
       });
+      this.renderDefaultCloseButton.emit(this.showRenderDefaultCloseButton);
+  }
+
+  onCloseModal() {
+    this.closeModal.emit(null);
   }
 
   ngOnDestroy() {
